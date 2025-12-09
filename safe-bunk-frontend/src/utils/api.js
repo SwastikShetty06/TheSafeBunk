5cb6cdcc6acab6ef57330e7ed6c17cf12a3b5775
Swastik Shetty <127413152+SwastikShetty06@users.noreply.github.com>
2025-12-09 11:58:57 +0530

    update api to live

--- safe-bunk-frontend/src/utils/api.js
+++ safe-bunk-frontend/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: import.meta.env.PROD
+        ? 'https://the-safe-bunk-4ngpzw1bq-swastikshetty06s-projects.vercel.app/api'
+        : 'http://localhost:5001/api',
 });
 
 // Add a request interceptor to add the token
